d69ec5edc30930a58b71676c5486a93f1f122742
Noé Valdez <nomx@msn.com>
2017-10-09 13:39:29 -0600

    Initial commit

---
+++ CoppelExamenB/Usuarios.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace examen
+{
+    partial class Usuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewCheckBoxColumn1 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.lblClave = new System.Windows.Forms.Label();
+            this.txtClave = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblFechaNacimiento = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.dtpFechaNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.usuarioDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.usuarioDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            // 
+            // dataGridViewCheckBoxColumn1
+            // 
+            this.dataGridViewCheckBoxColumn1.Name = "dataGridViewCheckBoxColumn1";
+            // 
+            // lblClave
+            // 
+            this.lblClave.AutoSize = true;
+            this.lblClave.Location = new System.Drawing.Point(41, 257);
+            this.lblClave.Name = "lblClave";
+            this.lblClave.Size = new System.Drawing.Size(37, 13);
+            this.lblClave.TabIndex = 2;
+            this.lblClave.Text = "Clave:";
+            // 
+            // txtClave
+            // 
+            this.txtClave.Location = new System.Drawing.Point(81, 254);
+            this.txtClave.MaxLength = 3;
+            this.txtClave.Name = "txtClave";
+            this.txtClave.Size = new System.Drawing.Size(44, 20);
+            this.txtClave.TabIndex = 1;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtNombre.Location = new System.Drawing.Point(81, 280);
+            this.txtNombre.MaxLength = 20;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(199, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(31, 283);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(47, 13);
+            this.lblNombre.TabIndex = 4;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtApellido.Location = new System.Drawing.Point(81, 306);
+            this.txtApellido.MaxLength = 20;
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(199, 20);
+            this.txtApellido.TabIndex = 3;
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(31, 313);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(47, 13);
+            this.lblApellido.TabIndex = 6;
+            this.lblApellido.Text = "Apellido:";
+            // 
+            // lblFechaNacimiento
+            // 
+            this.lblFechaNacimiento.AutoSize = true;
+            this.lblFechaNacimiento.Location = new System.Drawing.Point(12, 335);
+            this.lblFechaNacimiento.Name = "lblFechaNacimiento";
+            this.lblFechaNacimiento.Size = new System.Drawing.Size(66, 13);
+            this.lblFechaNacimiento.TabIndex = 8;
+            this.lblFechaNacimiento.Text = "Fecha Nac.:";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(420, 247);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 5;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(420, 308);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 6;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(420, 278);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 7;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // dtpFechaNacimiento
+            // 
+            this.dtpFechaNacimiento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFechaNacimiento.Location = new System.Drawing.Point(81, 334);
+            this.dtpFechaNacimiento.Name = "dtpFechaNacimiento";
+            this.dtpFechaNacimiento.Size = new System.Drawing.Size(83, 20);
+            this.dtpFechaNacimiento.TabIndex = 4;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(420, 337);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 9;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // usuarioDataGridView
+            // 
+            this.usuarioDataGridView.AllowUserToAddRows = false;
+            this.usuarioDataGridView.AllowUserToDeleteRows = false;
+            this.usuarioDataGridView.AllowUserToResizeColumns = false;
+            this.usuarioDataGridView.AllowUserToResizeRows = false;
+            this.usuarioDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.usuarioDataGridView.Location = new System.Drawing.Point(12, 12);
+            this.usuarioDataGridView.MultiSelect = false;
+            this.usuarioDataGridView.Name = "usuarioDataGridView";
+            this.usuarioDataGridView.ReadOnly = true;
+            this.usuarioDataGridView.RowHeadersVisible = false;
+            this.usuarioDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.usuarioDataGridView.Size = new System.Drawing.Size(483, 218);
+            this.usuarioDataGridView.TabIndex = 10;
+            this.usuarioDataGridView.MouseClick += new System.Windows.Forms.MouseEventHandler(this.usuarioDataGridView_MouseClick);
+            // 
+            // Usuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 376);
+            this.ControlBox = false;
+            this.Controls.Add(this.usuarioDataGridView);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.dtpFechaNacimiento);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.lblFechaNacimiento);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtClave);
+            this.Controls.Add(this.lblClave);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Usuarios";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ABC Usuarios - Borrado Físico";
+            this.Load += new System.EventHandler(this.Usuarios_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.usuarioDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblClave;
+        private System.Windows.Forms.TextBox txtClave;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblFechaNacimiento;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.DateTimePicker dtpFechaNacimiento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn dataGridViewCheckBoxColumn1;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.DataGridView usuarioDataGridView;
+    }
+}
+
---
+++ CoppelExamenB/Usuarios.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using examen.BLL;
+
+namespace examen
+{
+    public partial class Usuarios : Form
+    {
+        bool editando = false;
+        int usuarioid;
+
+        // ----------------------- METODOS ----------------------- //
+        public Usuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void limpiarTextos()
+        {
+            txtClave.Text = String.Empty;
+            txtNombre.Text = String.Empty;
+            txtApellido.Text = String.Empty;
+            dtpFechaNacimiento.Text = DateTime.Today.ToString();
+            editando = false;
+            txtClave.Focus();
+        }
+
+        private bool validaDatos()
+        {
+            if (txtClave.Text.Length < 3 || txtNombre.Text.Length < 3 || txtApellido.Text.Length < 3)
+            {
+                MessageBox.Show("Por favor, verifique que todos los campos tengan al menos 3 caracteres.", "Atención!");
+                return false;
+            }
+            return true;
+        }
+
+        private void cargarGrid()
+        {
+            UsuarioBLL obj = new UsuarioBLL();
+            this.usuarioDataGridView.DataSource = obj.obtenerUsuario();
+            usuarioDataGridView.Columns["UsuarioID"].Width = 60;
+            usuarioDataGridView.Columns["Clave"].Width = 40;
+            usuarioDataGridView.Columns["Nombre"].Width = 150;
+            usuarioDataGridView.Columns["Apellido"].Width = 150;
+            usuarioDataGridView.Columns["FechaNacimiento"].Width = 80;
+            usuarioDataGridView.Columns["FechaNacimiento"].HeaderText = "Fecha Nacimiento";
+        }
+
+        // ----------------------- EVENTOS ----------------------- //
+        private void Usuarios_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                cargarGrid();
+            }
+            catch
+            {
+                MessageBox.Show("Ocurrió un error al obtener los usuarios.","Error!");
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            UsuarioBLL usuario = new UsuarioBLL();
+            usuario.Clave = txtClave.Text;
+            usuario.Nombre = txtNombre.Text;
+            usuario.Apellido = txtApellido.Text;
+            usuario.FechaNacimiento = dtpFechaNacimiento.Text;
+            usuario.UsuarioID = usuarioid;
+
+            try
+            {
+                if (validaDatos())
+                {
+                    if (editando)
+                        usuario.actualizarUsuario(usuario);
+                    else
+                        usuario.guardarUsuario(usuario);
+
+                    limpiarTextos();
+                }
+
+                cargarGrid();
+            }
+            catch
+            {
+                MessageBox.Show("Ocurrió un error al intentar guardar.", "Error!");
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            UsuarioBLL usuario = new UsuarioBLL();
+            DialogResult eliminar;
+
+            try
+            {
+                if (editando)
+                {
+                    eliminar = MessageBox.Show("¿Está seguro que desea eliminar este registro?", "Eliminar Usuario", MessageBoxButtons.YesNo);
+                    if (eliminar == DialogResult.Yes)
+                    {
+                        usuario.eliminarUsuarioFisico(usuarioid);
+                        limpiarTextos();
+                        cargarGrid();
+                    }
+                }
+                else
+                    MessageBox.Show("Por favor, seleccione un registro a eliminar.", "Atención!");
+            }
+            catch
+            {
+                MessageBox.Show("Ocurrió un error al intentar eliminar.", "Error!");
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            limpiarTextos();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void usuarioDataGridView_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (usuarioDataGridView.RowCount > 0)
+            {
+                usuarioid = Convert.ToInt32(usuarioDataGridView.SelectedRows[0].Cells[0].Value.ToString());
+                txtClave.Text = usuarioDataGridView.SelectedRows[0].Cells[1].Value.ToString();
+                txtNombre.Text = usuarioDataGridView.SelectedRows[0].Cells[2].Value.ToString();
+                txtApellido.Text = usuarioDataGridView.SelectedRows[0].Cells[3].Value.ToString();
+                dtpFechaNacimiento.Text = usuarioDataGridView.SelectedRows[0].Cells[4].Value.ToString();
+                editando = true;
+            }
+            else
+                txtClave.Focus();
+        }
+    }
+}
